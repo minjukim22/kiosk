3c270e9a053b5069ac65f616f3a31174e4570b73
minjukim23 <117254280+minjukim23@users.noreply.github.com>
2023-06-13 23:22:52 +0900

    third commit

--- gomo/main.js
+++ gomo/main.js
@@ -15,9 +15,17 @@ function here() {
     document.getElementById("pay_detail").style.display = 'none';
     document.getElementById("screen_to_detail").style.display  = 'none';
 
-    document.getElementById("window_detail").style.display = 'none';
+    document.getElementById("window_detail1").style.display = 'none';
+    document.getElementById("window_detail2").style.display = 'none';
+    document.getElementById("window_detail3").style.display = 'none';
+    document.getElementById("window_detail4").style.display = 'none';
+    document.getElementById("window_detail5").style.display = 'none';
 
     document.getElementById("pay_new").style.display = 'none';
+
+    document.getElementById("fast_pay").style.display = 'none';
+    document.getElementById("general_pay").style.display = 'none';
+    document.getElementById("gift").style.display = 'none';
 
     reset();
 }
@@ -175,24 +183,55 @@ function sub_restore() {
     
 }
 
-function open_detail() {
-    document.getElementById("window_detail").style.display = 'block';
+function open_detail1() {
+    document.getElementById("window_detail1").style.display = 'block';
     document.getElementById("screen_to_detail").style.display  = 'block';
 }
 
+function open_detail2() {
+    document.getElementById("window_detail2").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+function open_detail3() {
+    document.getElementById("window_detail3").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+function open_detail4() {
+    document.getElementById("window_detail4").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+function open_detail5() {
+    document.getElementById("window_detail5").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+
 function back() {
-    document.getElementById("window_detail").style.display = 'none';
+    document.getElementById("window_detail1").style.display = 'none';
+    document.getElementById("window_detail2").style.display = 'none';
+    document.getElementById("window_detail3").style.display = 'none';
+    document.getElementById("window_detail4").style.display = 'none';
+    document.getElementById("window_detail5").style.display = 'none';
     document.getElementById("screen_to_detail").style.display  = 'none';
 
     document.getElementById("pay_detail").style.display = 'none';
     document.getElementById("pay_new").style.display  = 'none';
+    document.getElementById("fast_pay").style.display  = 'none';
+    document.getElementById("general_pay").style.display = 'none';
 
     reset();
 
 }
 
 function add() {
-    document.getElementById("window_detail").style.display = 'none';
+    document.getElementById("window_detail1").style.display = 'none';
+    document.getElementById("window_detail2").style.display = 'none';
+    document.getElementById("window_detail3").style.display = 'none';
+    document.getElementById("window_detail4").style.display = 'none';
+    document.getElementById("window_detail5").style.display = 'none';
     document.getElementById("screen_to_detail").style.display  = 'none';
 
     reset();
@@ -200,12 +239,13 @@ function add() {
 
 function reset() {
     var radio1 = document.querySelector('input[type=radio][name=temp]:checked');
+    radio1.checked = false;
     var radio2 = document.querySelector('input[type=radio][name=size]:checked');
     var radio3 = document.querySelector('input[type=radio][name=cream]:checked');
     var radio4 = document.querySelector('input[type=radio][name=syrup]:checked');
     var radio5 = document.querySelector('input[type=radio][name=bubble]:checked');
 
-    radio1.checked = false;
+    
     radio2.checked = false;
     radio3.checked = false;
     radio4.checked = false;
@@ -214,7 +254,11 @@ function reset() {
 
 function pay() {
     document.getElementById("pay_detail").style.display = 'none';
-    document.getElementById("window_detail").style.display = 'none';
+    document.getElementById("window_detail1").style.display = 'none';
+    document.getElementById("window_detail2").style.display = 'none';
+    document.getElementById("window_detail3").style.display = 'none';
+    document.getElementById("window_detail4").style.display = 'none';
+    document.getElementById("window_detail5").style.display = 'none';
 
     document.getElementById("screen_to_detail").style.display  = 'block';
     document.getElementById("pay_new").style.display  = 'block';
@@ -225,13 +269,13 @@ function open_pay() {
     document.getElementById("pay_detail").style.display = 'block';
     document.getElementById("screen_to_detail").style.display  = 'block';
 
-    write_order_list_window_pay(order_list);
+    order(order_list);
     
-    document.getElementById("w_total_number").innerText = total_list[0];
-    document.getElementById("w_total_price").innerText =total_list[1];
+    document.getElementById("total_number").innerText = total_list[0];
+    document.getElementById("total_price").innerText =total_list[1];
 }
 
-function Item(name, price) {
+function Menu(name, price) {
     this.name = name; 
     this.number = 0;
     this.price = parseInt(price);
@@ -240,10 +284,9 @@ function Item(name, price) {
 var order_list = [];
 function option(id, price) {
 
-    var order = new Item(id, price);
+    var order = new Menu(id, price);
     order.number += 1;
 
-    open_detail();
     cnt = 0
 
     for (i = 0; i < order_list.length; i++) {
@@ -257,11 +300,121 @@ function option(id, price) {
         order_list.push(order);
     }
 
-    open_order_list(order_list);
+    open_list(order_list);
+}
+
+function option1(id, price) {
+
+    var order = new Menu(id, price);
+    order.number += 1;
+
+    open_detail1();
+    cnt = 0
+
+    for (i = 0; i < order_list.length; i++) {
+        if (order.name == order_list[i].name) {
+            order_list[i].number += 1;
+            cnt += 1; 
+        }
+    }
+
+    if (cnt == 0 || order_list.length == 0) {
+        order_list.push(order);
+    }
+
+    open_list(order_list);
+}
+
+function option2(id, price) {
+
+    var order = new Menu(id, price);
+    order.number += 1;
+
+    open_detail2();
+    cnt = 0
+
+    for (i = 0; i < order_list.length; i++) {
+        if (order.name == order_list[i].name) {
+            order_list[i].number += 1;
+            cnt += 1; 
+        }
+    }
+
+    if (cnt == 0 || order_list.length == 0) {
+        order_list.push(order);
+    }
+
+    open_list(order_list);
+}
+
+function option3(id, price) {
+
+    var order = new Menu(id, price);
+    order.number += 1;
+
+    open_detail3();
+    cnt = 0
+
+    for (i = 0; i < order_list.length; i++) {
+        if (order.name == order_list[i].name) {
+            order_list[i].number += 1;
+            cnt += 1; 
+        }
+    }
+
+    if (cnt == 0 || order_list.length == 0) {
+        order_list.push(order);
+    }
+
+    open_list(order_list);
+}
+
+function option4(id, price) {
+
+    var order = new Menu(id, price);
+    order.number += 1;
+
+    open_detail4();
+    cnt = 0
+
+    for (i = 0; i < order_list.length; i++) {
+        if (order.name == order_list[i].name) {
+            order_list[i].number += 1;
+            cnt += 1; 
+        }
+    }
+
+    if (cnt == 0 || order_list.length == 0) {
+        order_list.push(order);
+    }
+
+    open_list(order_list);
+}
+
+function option5(id, price) {
+
+    var order = new Menu(id, price);
+    order.number += 1;
+
+    open_detail5();
+    cnt = 0
+
+    for (i = 0; i < order_list.length; i++) {
+        if (order.name == order_list[i].name) {
+            order_list[i].number += 1;
+            cnt += 1; 
+        }
+    }
+
+    if (cnt == 0 || order_list.length == 0) {
+        order_list.push(order);
+    }
+
+    open_list(order_list);
 }
 
 var total_list= [0, 0];
-function open_order_list(order_list) {
+function open_list(order_list) {
     var total_num = 0; 
     var total_price = 0; 
 
@@ -274,10 +427,67 @@ function open_order_list(order_list) {
     total_list[1] = total_price;
 }
 
-function write_order_list_window_pay (order_list) {
+function order (order_list) {
     for (i=0; i<order_list.length; i++) {
-        document.getElementById("w_order_" + (i + 1)).textContent = (order_list[i].name);
-        document.getElementById("w_number_" + (i + 1)).textContent = (order_list[i].number) + "개 " + (order_list[i].price) * (order_list[i].number) + "원";
+        document.getElementById("order_" + (i + 1)).textContent = (order_list[i].name);
+        document.getElementById("number_" + (i + 1)).textContent = (order_list[i].number) + "개 " + (order_list[i].price) * (order_list[i].number) + "원";
     }
 }
 
+function fastpay() {
+    document.getElementById("pay_new").style.display  = 'none';
+    document.getElementById("gift").style.display  = 'none';
+
+    document.getElementById("fast_pay").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+    document.getElementById("total").innerText = total_list[1]+"원";
+
+}
+
+function cancel_menu() {
+    document.getElementById("screen_to_detail").style.display  = 'none';
+
+    document.getElementById("fast_pay").style.display  = 'none';
+
+    alert("주문이 취소되었습니다.");
+}
+
+function approval() {
+    alert("결제가 완료되었습니다.");
+    location.href = "index.html";
+}
+
+function gpay() {
+    document.getElementById("pay_new").style.display  = 'none';
+    document.getElementById("gift").style.display  = 'none';
+
+    document.getElementById("general_pay").style.display = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+function next_step() {
+    document.getElementById("general_pay").style.display = 'none';
+
+    document.getElementById("gift").style.display  = 'block';
+    document.getElementById("screen_to_detail").style.display  = 'block';
+}
+
+function addInput(v){
+    let input = document.getElementById("input");
+    if(input.value=="0") {
+        input.value=v;
+    }
+    else {
+        input.value += v;
+    }
+}
+function backward() {
+    let input = document.getElementById("input");
+    let len = input.value.length;
+    if(len == 0) {
+        return;
+    }
+    else {
+        input.value = input.value.substr(0, len-1); 
+    }
+}
